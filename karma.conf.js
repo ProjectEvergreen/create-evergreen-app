f5b64752a0ec20f649721f6bf20cbf08c9188fc9
Owen Buckley <owenbuckley13@gmail.com>
2018-10-21 14:42:29 -0400

    clean up config file

--- karma.conf.js
+++ karma.conf.js
@@ -24,10 +24,7 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     files: [
-      // { pattern: './node_modules/query-selector-shadow-dom/dist/querySelectorShadowDom.js', watched: false },
       { pattern: './karma-test-shim.js', watched: false }
-      // { pattern: './node_modules/query-selector-shadow-dom/dist/querySelectorShadowDom.js', watched: false }
-      // { pattern: './node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js', watched: false }
     ],
     preprocessors: {
       './karma-test-shim.js': ['webpack', 'sourcemap']
