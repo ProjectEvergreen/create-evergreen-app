025a78b648b2c7436af9ef6f408179afc1fe263a
HutchGrant <h.g.utchinson@gmail.com>
2018-11-07 19:59:16 -0500

    Removing formatting

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,6 +2,7 @@ import { html, LitElement } from '@polymer/lit-element';
 import css from './home.css';
 
 class HomePageComponent extends LitElement {
+
   render() {
     return html`
       <style>
