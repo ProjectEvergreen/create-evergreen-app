88ca0f22b76ad3150a622d52060ccc9c60b98c1a
HutchGrant <h.g.utchinson@gmail.com>
2019-02-26 21:29:15 -0500

    chore: updating dependencies

--- template/src/pages/home/home.js
+++ template/src/pages/home/home.js
@@ -1,4 +1,4 @@
-import { html, LitElement } from '@polymer/lit-element';
+import { html, LitElement } from 'lit-element';
 import css from './home.css';
 
 class HomePageComponent extends LitElement {
